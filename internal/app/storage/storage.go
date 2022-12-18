1e682291793598c150ee31de95869dd2960608b1
Alexeyka <alexeykaz11@gmail.com>
2022-12-18 16:00:27 +0300

    new file:   internal/app/storage/database.txt 	modified:   internal/app/storage/storage.go

--- internal/app/storage/storage.go
+++ internal/app/storage/storage.go
@@ -1,19 +1,37 @@
 package storage
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
+	"log"
 	"math/rand"
+	"os"
 	"strconv"
 	"sync"
 )
 
 type URLrecord map[string]string
+type URLJSONrecord struct {
+    ID string `json:"id"`
+    FullURL string `json:"fullurl"`    
+}
 
 var URL URLrecord
+var dbPath string
+var RAMonly, dbPathexists bool
 var onlyOnce sync.Once
 
 func Initdb() {
-	URL = make(URLrecord)
+	dbPath, dbPathexists = os.LookupEnv("FILE_STORAGE_PATH")
+	if (dbPathexists && dbPath != "") {
+		RAMonly = false
+		fmt.Println("Loading DB file...")
+		} else {
+			RAMonly = true
+			fmt.Println("DB file path is not set in env vars! Loading RAM storage...")
+			URL = make(URLrecord)
+	}	
 	fmt.Println("Storage ready!")
 }
 
@@ -24,8 +42,29 @@ func Storerecord(fullURL string) string{
 	for (!isnewID(id)){
 		id = strconv.Itoa(rand.Intn(9999))
 	}
+
+	if RAMonly {
 		URL[id] = fullURL
-		return id
+	} else {
+		URLJSONline := URLJSONrecord{
+			ID:			id,
+        	FullURL:	fullURL,
+		}
+		JSONdata, err := json.Marshal(&URLJSONline)
+		if err != nil {
+			return err.Error()
+		}
+		JSONdata = append(JSONdata, '\n')
+		log.Println("dbPath: " + dbPath)
+
+		DBfile, _ := os.OpenFile(dbPath, os.O_RDWR|os.O_APPEND , 0777)
+		_, err = DBfile.Write(JSONdata)	
+		if err != nil {	
+			return err.Error()
+		}
+		DBfile.Close()
+	}
+	return id
 }
 
 func Getrecord(id string) string {
@@ -40,8 +79,40 @@ func Getrecord(id string) string {
 }
 
 func isnewID(id string) bool{
+	if RAMonly {
 	result := URL[id]
 	if (result == ""){
 		return true
 	} else {return false}
+	}else {
+		DBfile, err := os.OpenFile(dbPath, os.O_RDONLY, 0777)
+		if err != nil {
+			panic(err)
+		}
+		scanner := bufio.NewScanner(DBfile)
+		line :=0
+		var postJSON URLJSONrecord
+		var idIsnew bool
+		idIsnew = true
+		for scanner.Scan() && idIsnew{
+			//log.Println(line)
+			//log.Println("lineStr: " + scanner.Text())
+			if scanner.Text() != "" {
+				err = json.Unmarshal([]byte(scanner.Text()), &postJSON)
+				if err != nil {
+					panic(err)
+				}
+			//отладка что было в поле FullURL в строке файла
+				log.Println(postJSON.ID)
+				log.Println(postJSON.FullURL)
+				if postJSON.ID == id {
+					idIsnew = false
+					log.Println("ID exists: " + postJSON.ID)
+				}
+				line++
+			}
+		}
+		DBfile.Close()
+		return idIsnew
+	}
 }
