6fb0a30d5af62f9a17f21f23c94dd247df26ea8b
Alexeyka <alexeykaz11@gmail.com>
2022-12-18 23:46:50 +0300

    modified:   cmd/shortener/main.go 	modified:   internal/app/server/initconfig.go

--- internal/app/server/initconfig.go
+++ internal/app/server/initconfig.go
@@ -10,12 +10,17 @@ var FileDBpath, BaseURL, SrvAddress string
 
 func InitFlags() {
 
+	SrvAddressflag := flag.String("a", "127.0.0.1:8080", "SERVER_ADDRESS flag")
+	BaseURLflag := flag.String("b", "http://127.0.0.1:8080", "BASE_URL flag")
+	FileDBpathflag := flag.String("f", "../../internal/app/storage/database.txt", "FILE_STORAGE_PATH flag")
+
+	flag.Parse()
+
 	baseURLENV, baseURLexists := os.LookupEnv("BASE_URL")
 	srvAddressENV, srvAddressexists := os.LookupEnv("SERVER_ADDRESS")
 	fileDBpathENV, fileDBpathexists := os.LookupEnv("FILE_STORAGE_PATH")
 
-	if !srvAddressexists{
-		SrvAddressflag := flag.String("a", "127.0.0.1:8080", "SERVER_ADDRESS")
+	if !srvAddressexists{		
 		SrvAddress = *SrvAddressflag
 		fmt.Println("Set from flag: SrvAddress:", SrvAddress)
 	} else {
@@ -23,8 +28,7 @@ func InitFlags() {
 		fmt.Println("Set from ENV: SrvAddress:", SrvAddress)
 	}
 
-	if !baseURLexists{
-		BaseURLflag := flag.String("b", "http://127.0.0.1:8080", "BASE_URL")
+	if !baseURLexists{		
 		BaseURL = *BaseURLflag
 		fmt.Println("Set from flag: BaseURL:", BaseURL)
 	} else {
@@ -50,7 +54,7 @@ func InitFlags() {
 			i++
 		}
 		if flagFound {
-			FileDBpathflag := flag.String("f", "../../internal/app/storage/database.txt", "FILE_STORAGE_PATH")
+			//FileDBpathflag := flag.String("f", "../../internal/app/storage/database.txt", "FILE_STORAGE_PATH")
 			FileDBpath = *FileDBpathflag
 			fmt.Println("Set from flag: FileDBpath:", FileDBpath)
 		} else {
