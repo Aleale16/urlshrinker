fd2191060a999238126ad0e322ed767efbf6ee19
Alexeyka <alexeykaz11@gmail.com>
2023-01-22 12:28:34 +0300

    modified:   internal/app/server/server.go

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -36,7 +36,8 @@ func Start(){
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Compress(5, "gzip"))
 	
-	r.Get("/", handler.GetHandler)
+	//reqstring:= chi.URLParam(r)
+	r.Get("/{id}", handler.GetHandler)
 	r.Get("/api/user/urls", handler.GetUsrURLsHandler)
 	r.Get("/ping", handler.GetPingHandler)
 
