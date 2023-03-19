56e3d258e67b4f652bd600df5d054baf0a235493
Alexeyka <alexeykaz11@gmail.com>
2023-03-19 14:43:40 +0300

    modified:   cmd/shortener/main.go 	new file:   internal/app/initconfig/initbuildinfo.go

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,7 +1,10 @@
+//go:build !darwin
+
 package main
 
 import (
 	"flag"
+	"fmt"
 
 	"urlshrinker/internal/app/initconfig"
 	"urlshrinker/internal/app/server"
@@ -9,7 +12,32 @@ import (
 	//"github.com/Aleale16/urlshrinker/internal/app/server"
 )
 
+// Init global initbuildinfo strings
+// Output:
+// Build version: <buildVersion> (or "N/A" if no value)
+// Build date: <buildDate> (or "N/A" if no value)
+// Build commit: <buildCommit> (or "N/A" if no value)
+var (
+	// buildVersion - global buildVersion value.
+	buildVersion  string
+	// buildDate - global buildDate value.
+	buildDate  string
+	// buildCommit - buildCommit value.
+	buildCommit  string
+)
+
 func main() {
+
+
+	// fmt.Printf("Build version: %s", initconfig.BuildVersion)
+	// fmt.Printf("Build date: %s", initconfig.BuildDate)
+	// fmt.Printf("Build commit: %s", initconfig.BuildCommit)
+	if buildVersion == "" {buildVersion = "N/A"}
+	if buildDate == "" {buildDate = "N/A"}
+	if buildCommit == "" {buildCommit = "N/A"}
+	fmt.Printf("Build version: %s", buildVersion)
+	fmt.Printf("Build date: %s", buildDate)
+	fmt.Printf("Build commit: %s", buildCommit)
 
 	//staticlint.Runinc18Checks()
 
---
+++ internal/app/initconfig/initbuildinfo.go
@@ -0,0 +1,15 @@
+package initconfig
+
+// Init global initbuildinfo strings
+// Output:
+// Build version: <buildVersion> (or "N/A" if no value)
+// Build date: <buildDate> (or "N/A" if no value)
+// Build commit: <buildCommit> (or "N/A" if no value)
+// var (
+// 	// buildVersion - global buildVersion value.
+// 	BuildVersion  string
+// 	// buildDate - global buildDate value.
+// 	BuildDate  string
+// 	// buildCommit - buildCommit value.
+// 	BuildCommit  string
+// )
